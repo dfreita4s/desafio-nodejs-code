f2ee2ce15cb5aa39e14816ccf61017c199678d22
dfreita4s <danifp19@gmail.com>
2022-09-27 21:39:33 -0300

    Combati um bom combate !

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocs from './swagger.json'
 
 import { AppDataSource } from "./data-source"
 import routes from './routes'
@@ -7,6 +9,8 @@ AppDataSource.initialize().then(()=> {
     const app = express()
 
     app.use(express.json())
+
+    app.use("/documentation", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
     app.use(routes);
     
--- src/routes.ts
+++ src/routes.ts
@@ -9,19 +9,19 @@ const routes = Router();
 routes.post('/login', new MemberController().login)
 
 routes.post('/member/create', authMiddleware, new MemberController().create);
-routes.get('/member/:id', new MemberController().view);
-routes.delete('/member/:id', authMiddleware, new MemberController().remove);
+routes.get('/member/view/:id', new MemberController().view);
+routes.delete('/member/delete/:id', authMiddleware, new MemberController().remove);
 routes.post('/member/update/:id', authMiddleware, new MemberController().update);
 
 routes.post('/role/create', authMiddleware, new RoleController().create);
-routes.get('/role/:id', new RoleController().view);
-routes.delete('/role/:id', authMiddleware, new RoleController().remove);
+routes.get('/role/view/:id', new RoleController().view);
+routes.delete('/role/delete/:id', authMiddleware, new RoleController().remove);
 routes.post('/role/update/:id', authMiddleware, new RoleController().update)
 
 
 routes.post('/department/create', authMiddleware, new DepartmentController().create);
-routes.get('/department/:id', new DepartmentController().view);
-routes.delete('/department/:id', authMiddleware, new DepartmentController().remove);
+routes.get('/department/view/:id', new DepartmentController().view);
+routes.delete('/department/delete/:id', authMiddleware, new DepartmentController().remove);
 routes.post('/department/update/:id', authMiddleware, new DepartmentController().update)
 
 export default routes;
